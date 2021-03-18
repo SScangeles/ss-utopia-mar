c42d6894d0fe51907b196f0d009ad2b92c5a2a76
christian <christian.angeles@smoothstack.com>
2021-03-17 21:00:31 -0700

    Initial commit

---
+++ src/main/java/com/ss/utopia/console/UtopiaConsole.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ss.utopia.console;
+
+/**
+ * @author Christian Angeles
+ *
+ */
+public class UtopiaConsole {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+
+	}
+
+}
